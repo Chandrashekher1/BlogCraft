4e413a559532f5e24fc473d827ee864ab1a8a3fd
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-30 14:29:56 +0530

    add postcard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Postcard = () => {
   return (
-    <div className=''>
-      <div className=' w-80 border border-gray-100 shadow-lg'>
+    <div className=' flex flex-wrap mx-16 my-16'>
+      <div className=' w-80 border border-indigo-600 shadow-2xl shadow-cyan-800 mx-4 my-4'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGB5p3OgOHj0lchXkXzbBFpmntcAqsu_EACA&s" alt="" />
+        <div className='my-4'>
+          <h1>title</h1>
+          <p className='my-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui hic asperiores est ex, rerum eligendi pariatur esse dolorum quia fugiat.</p>
+        </div>
+      </div>
+      <div className=' w-80 border border-indigo-600 shadow-2xl shadow-cyan-800 mx-4 my-4'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGB5p3OgOHj0lchXkXzbBFpmntcAqsu_EACA&s" alt="" />
+        <div className='my-4'>
+          <h1>title</h1>
+          <p className='my-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui hic asperiores est ex, rerum eligendi pariatur esse dolorum quia fugiat.</p>
+        </div>
+      </div>
+      <div className=' w-80 border border-indigo-600 shadow-2xl shadow-cyan-800 mx-4 my-4'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGB5p3OgOHj0lchXkXzbBFpmntcAqsu_EACA&s" alt="" />
+        <div className='my-4'>
+          <h1>title</h1>
+          <p className='my-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui hic asperiores est ex, rerum eligendi pariatur esse dolorum quia fugiat.</p>
+        </div>
+      </div>
+      <div className=' w-80 border border-indigo-600 shadow-2xl shadow-cyan-800 mx-4 my-4'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGB5p3OgOHj0lchXkXzbBFpmntcAqsu_EACA&s" alt="" />
         <div className='my-4'>
           <h1>title</h1>
