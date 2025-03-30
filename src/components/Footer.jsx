492e02023fd147b266fa7b448738fa3ce507de19
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-30 14:28:53 +0530

    add all the footer data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import SocialIcons from './SocialIcon'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex md:justify-between px-4 border border-t border-indigo-800 py-4'>
+        <div><h1 className='font-bold text-2xl flex cursor-pointer'>CP <p className='text-blue-700'>03</p></h1></div>
+        <div>
+            <p className='md:font-semibold md:text-xl'>Created by Cp03 <span>, @2025</span></p>
+        </div>
+        <div>
+            <ul className=''>
+                <SocialIcons/>
+            </ul>            
+        </div>
+    </div>
   )
 }
 
