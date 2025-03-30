90e2dfe89977e75ada5699d0d012d11b0204cdf0
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-30 14:29:23 +0530

    complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div className='flex justify-between p-4  shadow-md'>
+    <div className='flex justify-between p-4  shadow-md sticky top-0 bg-black'>
       <div>
-        <h1>LOGO</h1>
+        <h1 className='font-bold text-2xl cursor-pointer flex'>CP <p className='text-blue-700'>03</p></h1>
       </div>
       <div>
         <Navbar/>
