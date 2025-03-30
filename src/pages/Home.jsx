b8c30e7bd9b69a2a2d91c7c2d1de3ab5f8c31858
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-30 14:26:18 +0530

    sample post card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,19 @@ import Postcard from '../components/Postcard'
 
 const Home = () => {
   return (
-    <div>
-      <Postcard/>
-    </div>
+    <>
+      <div className='text-center my-16 text-2xl'>
+        <h1 className='md:text-4xl text-3xl font-bold my-2'>Lorem ipsum dolor sit amet.</h1>
+        <p className='font-semibold text-md text-gray-400'>Create a unique and beautiful blog easily.</p>
+
+        <button className='my-4 border border-cyan-500 p-4 cursor-pointer shadow-md shadow-cyan-600 active:scale-95'>CREATE YOUR BLOG</button>
+      </div>
+      <div>
+        <Postcard/>
+      </div>
+    </>
+    
+    
   )
 }
 
