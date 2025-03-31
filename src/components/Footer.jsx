af1d69a2ba5a642dd43601f8e7cf51631fe49fbb
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-31 11:11:00 +0530

    change responsivness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
     <footer className="flex flex-col md:flex-row items-center justify-between px-6 py-6 border-t border-blue-800 bg-black text-white">
       <div className="flex items-center space-x-1 cursor-pointer">
         <h1 className="font-bold text-2xl">CP</h1>
-        <p className="text-blue-500 font-semibold text-2xl">03</p>
+        <p className="text-blue-500 font-bold text-2xl">03</p>
       </div>
 
       <div className="text-center md:text-left">
         <p className="font-medium text-lg">
-          Created by Cp03 <span className="text-gray-400">@2025</span>
+          Created by Chandrashekher <span className="text-gray-400">@2025</span>
         </p>
       </div>
 
-      <div className="flex space-x-4 mt-4 md:mt-0">
+      <div className="flex  mt-4 md:mt-0">
         <SocialIcons />
       </div>
     </footer>
