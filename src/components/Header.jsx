528682420e842d1ea65c9946faf8d5ca3820833c
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-31 11:12:26 +0530

    intergrate scrollToTop features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
   const navigate = useNavigate()
+  const [isShrunk, setIsShrunk] = useState(false);
+
   const handleLogo = () => {
       navigate("/")
   }
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsShrunk(true);
+            } else {
+                setIsShrunk(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    
 
   return (
-    <div className='flex justify-between p-4  shadow-md sticky top-0 bg-black'>
+    <div className={`flex justify-between p-4  shadow-md sticky top-0 bg-black ${isShrunk ? 'z-50':'null' }`}>
       <div>
         <h1 className='font-bold text-2xl cursor-pointer flex' onClick={handleLogo} >CP <p className='text-blue-700'>03</p></h1>
       </div>
