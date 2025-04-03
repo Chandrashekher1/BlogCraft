46d5fd7e616023d7fad128fa84398806dab33daf
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-03 18:23:05 +0530

    handle postid by per post

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { ShimmerPostItem } from 'react-shimmer-effects';
+import { post_API } from '../utils/constant';
+import { useNavigate } from 'react-router-dom';
 
-const Postcard = () => {
+const Postcard = () => {  
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const navigate = useNavigate();
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://cp-blog.onrender.com/api/post");
-
+        const response = await fetch(post_API);
         if (!response.ok) {
           throw new Error("Failed to fetch posts");
         }
-
         const json = await response.json();
         setData(json);
       } catch (error) {
@@ -28,6 +28,10 @@ const Postcard = () => {
 
   const shimmerCount = data.length > 0 ? data.length : 6;
 
+  const handlePostClick = (postId) => {
+    navigate(`/post-view/${postId}`);
+  };
+
   return (
     <div className="flex flex-wrap justify-center my-12 px-4 md:px-16 bg-black min-h-screen py-10">
       {loading ? (
@@ -36,15 +40,17 @@ const Postcard = () => {
             key={index} 
             className="w-full sm:w-80 m-4 p-6 bg-gray-800 rounded-xl shadow-lg h-80 animate-pulse"
           >
-            {/* <ShimmerPostItem card title cta height={250} /> */}
-
           </div>
         ))
       ) : data.length > 0 ? (
-        data.map((post, index) => (
-          <div key={index} className="border border-gray-700 bg-gray-900 bg-opacity-70 
+        data.map((post) => (
+          <div 
+            key={post._id} 
+            onClick={() => handlePostClick(post._id)} 
+            className="border border-gray-700 bg-gray-900 bg-opacity-70 
             p-6 m-4 shadow-lg backdrop-blur-md w-full sm:w-80 rounded-xl transition-all 
-            hover:scale-105 hover:shadow-xl cursor-pointer text-white active:scale-100">
+            hover:scale-105 hover:shadow-xl cursor-pointer text-white active:scale-100"
+          >
             <img 
               src="https://www.hexaphortechnologies.co.in/service_img/1687797021.png" 
               alt="Post Thumbnail" 
