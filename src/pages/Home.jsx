27245a996397d27d66f3e8404661c41eadb279be
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-06-23 20:06:23 +0530

    refactor: clean up profile page post editing login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,22 +9,27 @@ const Home = () => {
 
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center text-center py-12 bg-gradient-to-r from-blue-200 to-purple-500">
-        <h1 className="md:text-5xl text-3xl font-extrabold my-4 drop-shadow-lg">
-        
+    <div className='flex flex-col items-center'>
+      <div className="flex min-h-[700px] my-4 flex-col bg-cover bg-center bg-no-repeat items-start justify-center  px-4 w-[80%] rounded-md" 
+        style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.4)), url("https://lh3.googleusercontent.com/aida-public/AB6AXuB9yybwz4Ta5lROke_QkCfCtWvCBgXQQjVjJJ265P2_L7MqkGtHyWwf_8-vp05rJdBNTrrB18Ex11GxWXPT3-kq2N30KAfWND5ERgEXZPJlJBfcyxgSXdlHk9JpcfD-DhpcDxZ37Ng0N71u9SsDsKWRiS2rtAXmEjSzaIbSM3g1NcZWO96ppHeprMOe4Ly6sldAReZ-ZhAxDt0_xUurerGaOyEPNBxip7RxbXJvgUnB4s4l02PfNYjRfemhPF9kYshBBuE5R1tnbCP6")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        }}
+      >
+        <h1 className="md:text-4xl text-3xl font-extrabold mt-96 drop-shadow-lg mx-4">
           Share Your Thoughts with the World
         </h1>
-        <p className="font-medium text-lg md:text-2xl  text-gray-200">
+        <p className="font-medium text-lg md:text-2xl  text-gray-200 mt-4 mx-4">
           Create a unique and beautiful blog easily.
         </p>
 
-        <button className="mt-6 border text-cyan-600 font-bold text-xl px-6 py-3 rounded-lg cursor-pointer shadow-blue-700
-           hover:text-white transition-all duration-300 shadow-md active:scale-95" onClick={() => navigate("/create-post")}>
+        <button className="mt-6 bg-blue-600 hover:bg-blue-500 font-semibold text-xl px-6 py-3 rounded-lg cursor-pointer mx-4
+            transition-all duration-300 shadow-md active:scale-95" onClick={() => navigate("/create-post")}>
           CREATE YOUR BLOG
         </button>
       </div>
-      <motion.div className='md:px-40 my-16 mx-4'  whileHover={{
+      {/* <motion.div className='md:px-40 my-16 mx-4'  whileHover={{
         scale: 1.05,
         rotateX: 10,
         rotateY: -10,
@@ -35,12 +40,13 @@ const Home = () => {
           alt="Home"
           className='shadow-2xl shadow-blue-400 bg-gradient-to-r from-blue-500 to-purple-500  '
           />
-      </motion.div>
+      </motion.div> */}
 
-      <div className="">
+      <div className="mx-24">
+        <h1 className='font-semibold text-3xl mx-20 mt-4'>Featured Posts</h1>
         <Postcard/>
       </div>
-    </>
+    </div>
   );
 };
 
