85b30c694fa735b01d4c6d836e59d615cbd81fd5
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-06-23 20:03:27 +0530

    style: add new layout

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -26,40 +26,51 @@ const Postcard = () => {
 
     fetchData();
   }, []);
-
-  const shimmerCount = data.length > 0 ? data.length : 6;
-
+  
+  const shimmerCount = data.length > 0 ? data.length : 4;
   const handlePostClick = (postId) => {
     navigate(`/post-view/${postId}`);
   };
 
   return (
-    <div className="flex flex-wrap justify-center my-12 px-4 md:px-16 bg-black min-h-screen py-10">
+    <div className="flex flex-wrap justify-center px-4 md:px-16 bg-black min-h-screen py-10">
       {loading ? (
         Array.from({ length: shimmerCount }).map((_, index) => (
           <div 
             key={index} 
-            className="w-full sm:w-80 m-4 p-6 bg-gray-800 rounded-xl shadow-lg h-80 animate-pulse"
+            className="md:w-full  w-80 h-60 m-4 p-6 bg-gray-800 rounded-xl shadow-lg animate-pulse flex justify-between flex-col-reverse md:flex-row"
           >
+          <div>
+            <div className='bg-gray-400 h-2 w-40 my-2 rounded-lg'></div>
+            <div className='bg-gray-400 h-4 w-60 rounded-lg'></div>
+            <div className='bg-gray-400 h-4 md:w-80 w-40 rounded-lg my-2'></div>
+            <div className='bg-gray-400 h-4 w-60 rounded-lg'></div>
+            <button className='rounded-md py-2 px-8 bg-gray-500'></button>
+          </div>
+          <div className='md:h-40 md:w-80 w-60 h-60 bg-gray-500 rounded-md'></div>
           </div>
         ))
       ) : data.length > 0 ? (
         data.map((post) => (
           <div 
             key={post._id} 
-            onClick={() => handlePostClick(post._id)} 
-            className="border border-gray-700 bg-gray-900 bg-opacity-70 h-[60%]
-            p-6 m-4 shadow-lg backdrop-blur-md w-full sm:w-80 rounded-xl transition-all 
-            hover:scale-105 hover:shadow-xl cursor-pointer text-white active:scale-100"
+            className="border flex justify-between md:flex-row flex-col-reverse border-gray-700 bg-gray-900 bg-opacity-70 
+            p-6 m-4 shadow-lg backdrop-blur-md w-80 md:w-full rounded-xl transition-all 
+            hover:scale-105 hover:shadow-xl text-white"
           >
+            <div>
+              <p className='text-gray-400'>Featured</p>
+              <h1 className="text-lg sm:text-xl font-bold mt-4">{post.title}</h1>
+              <p className="mt-2 text-gray-300 ">{post.content.slice(0, 100)}...</p>
+              <h2 className="text-sm text-gray-400 mt-1">By {post.author}</h2>
+              <button className='border border-cyan-600 px-2 py-2 rounded-md my-4 active:scale-105 cursor-pointer'  onClick={() => handlePostClick(post._id)} >Read Post</button>
+
+            </div>
             <img 
               src={post?.image?.length > 0 ? post.image[0] : "https://www.hexaphortechnologies.co.in/service_img/1687797021.png"}
               alt="Post Thumbnail" 
-              className="w-full h-40 object-cover rounded-lg"
+              className="w-120 h-60 object-cover rounded-lg"
             />
-            <h1 className="text-lg sm:text-xl font-bold mt-4">{post.title}</h1>
-            <h2 className="text-sm text-gray-400 mt-1">By {post.author}</h2>
-            <p className="mt-2 text-gray-300 ">{post.content.slice(0, 100)} <span className='text-lg text-cyan-700'>... Read more...</span></p>
           </div>
         ))
       ) : (
