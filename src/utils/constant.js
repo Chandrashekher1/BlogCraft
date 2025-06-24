15fb3b8667d7deb6fe933e45ebd0695c175869a2
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-06-24 08:42:59 +0530

    change bg image

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,3 +5,4 @@ export const profile_APi = "https://cp-blog.onrender.com/api/user/me"
 export const allPost_API = "https://cp-blog.onrender.com/api/post/user"
 export const sample_image = "https://marketinginsidergroup.com/wp-content/uploads/2023/09/shutterstock_234456937-scaled-1.jpg"
 export const Backgorund_image= "https://res.cloudinary.com/dt9a9xhz1/image/upload/v1750730811/Blog1_sq8rzq.png"
+export const GPT_KEY = import.meta.env.VITE_GPT_API_KEY
