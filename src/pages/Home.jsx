a0ad0d9534851edb30f35dcf7996cfe55f8ad7be
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-06-24 08:33:00 +0530

    feat:responsive layout for home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,22 +7,22 @@ import { Backgorund_image } from '../utils/constant';
 const Home = () => {
   const navigate = useNavigate()
   return (
-    <div className='flex flex-col items-center'>
-      <div className="flex min-h-[700px] my-4 flex-col bg-cover bg-center bg-no-repeat items-start justify-center  px-4 w-[80%] rounded-md" 
+    <div className='flex flex-col items-center overflow-hidden'>
+      <div  className="flex flex-col justify-center items-start w-[80%] rounded-md mt-4 min-h-[500px] sm:min-h-[600px] md:min-h-[600px] bg-no-repeat bg-cover bg-center text-white px-4 sm:px-6 lg:px-12" 
         style={{
         backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.4)), url(${Backgorund_image})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         }}
       >
-        <h1 className="md:text-4xl text-3xl font-extrabold mt-96 drop-shadow-lg mx-4">
+        <h1 className="md:text-4xl text-3xl font-extrabold mt-80 drop-shadow-lg mx-4">
           Share Your Thoughts with the World
         </h1>
         <p className="font-medium text-lg md:text-2xl  text-gray-200 mt-4 mx-4">
           Create a unique and beautiful blog easily.
         </p>
 
-        <button className="mt-6 bg-blue-600 hover:bg-blue-500 font-semibold text-xl px-6 py-3 rounded-lg cursor-pointer mx-4
+        <button className="mt-6 bg-blue-600 hover:bg-blue-500 font-semibold md:text-xl px-6 py-3 rounded-lg cursor-pointer mx-4 mb-4
             transition-all duration-300 shadow-md active:scale-95" onClick={() => navigate("/create-post")}>
           CREATE YOUR BLOG
         </button>
@@ -40,8 +40,8 @@ const Home = () => {
           />
       </motion.div> */}
 
-      <div className="mx-24">
-        <h1 className='font-semibold text-3xl mx-20 mt-4'>Featured Posts</h1>
+      <div className="mx-24 mt-16">
+        <h1 className='font-semibold text-3xl mx-20 md:mt-4 mt-16'>Featured Posts</h1>
         <Postcard/>
       </div>
     </div>
