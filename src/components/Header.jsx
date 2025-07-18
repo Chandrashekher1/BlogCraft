4eb12353959e3d54fe5b0981501f5bd8595c6b47
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-19 01:41:49 +0530

    style: add margin to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,12 +24,10 @@ const Header = () => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
-    
-
   return (
     <div className={`flex justify-between p-4 md:px-16  shadow-md sticky top-0 bg-gray-950  border-b border-b-gray-800 ${isShrunk ? 'z-50':'null' }`}>
       <div className=''>
-        <h1 className='font-bold text-xl md:text-2xl cursor-pointer flex' onClick={handleLogo} ><span className='bg-gradient-to-r from-white to-gray-900 rounded-md mx-2'><TbCircleLetterB className='text-black text-2xl md:text-4xl'/></span> <p className='bg-gradient-to-r from-white to-gray-700 bg-clip-text text-transparent'>BlogCraft</p></h1>
+        <h1 className='font-bold text-xl md:text-2xl cursor-pointer flex mt-1' onClick={handleLogo} ><span className='bg-gradient-to-r from-white to-gray-900 rounded-md mx-2'><TbCircleLetterB className='text-black text-2xl md:text-4xl'/></span> <p className='bg-gradient-to-r from-white to-gray-700 bg-clip-text text-transparent'>BlogCraft</p></h1>
       </div>
       <div className=''>
         <Navbar/>
