39f5ee08f188da551e4002ab6ab5426b45d058c1
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-18 23:12:41 +0530

    style: change logo style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import { useNavigate } from 'react-router-dom'
+import { TbCircleLetterB } from "react-icons/tb";
+import { BsStars } from 'react-icons/bs';
 
 const Header = () => {
   const navigate = useNavigate()
@@ -25,11 +27,11 @@ const Header = () => {
     
 
   return (
-    <div className={`flex justify-between p-4  shadow-md sticky top-0 bg-black border-b border-b-gray-800 ${isShrunk ? 'z-50':'null' }`}>
-      <div>
-        <h1 className='font-bold text-2xl cursor-pointer flex' onClick={handleLogo} >Vibe<p className='text-blue-700'>Script</p></h1>
+    <div className={`flex justify-between p-4 md:px-16  shadow-md sticky top-0 bg-gray-950  border-b border-b-gray-800 ${isShrunk ? 'z-50':'null' }`}>
+      <div className=''>
+        <h1 className='font-bold text-xl md:text-2xl cursor-pointer flex' onClick={handleLogo} ><span className='bg-gradient-to-r from-white to-gray-900 rounded-md mx-2'><TbCircleLetterB className='text-black text-2xl md:text-4xl'/></span> <p className='bg-gradient-to-r from-white to-gray-700 bg-clip-text text-transparent'>BlogCraft</p></h1>
       </div>
-      <div>
+      <div className=''>
         <Navbar/>
       </div>
     </div>
