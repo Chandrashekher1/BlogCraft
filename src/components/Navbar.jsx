e94f195c250f8f252907d10ead886ac1cb9b49d1
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-18 23:13:58 +0530

    feat: add profile image at header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { CiPen } from "react-icons/ci";
+import { FiUser } from "react-icons/fi";
+import { MdOutlineWbSunny } from "react-icons/md";
 
 const Navbar = () => {
   const [token, setToken] = useState(localStorage.getItem("authorization"))
   const navigate = useNavigate()
   const topRef = useRef(null)
+  const userImage = localStorage.getItem('userImage')
   useEffect(() => {
     const handleStorageChange = () => {
       setToken(localStorage.getItem("authorization"));
@@ -28,30 +32,50 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black text-white font-semibold shadow-md sticky top-0 z-50">
+    <nav className="text-white font-semibold shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         {/* <Link to="/" className="text-xl font-bold">MyBlog</Link> */}
         <ul className="hidden md:flex space-x-6 text-lg">
           
+          <button>
+            <li className='cursor-pointer' onClick={scrollToTop} ref={topRef}><MdOutlineWbSunny style={{}}/></li>
+          </button>
+          
           <Link to="/">
-            <li className='cursor-pointer hover:border border-cyan-600 px-3 py-2 rounded-lg' onClick={scrollToTop} ref={topRef}>Home</li>
-          </Link>
-          <Link to="/">
-            <li className='cursor-pointer hover:border border-cyan-600 px-3 py-2 rounded-lg' onClick={scrollToTop} ref={topRef}>About</li>
+            <li className='cursor-pointer border border-transparent hover:border-white px-6 py-2 rounded-lg' onClick={scrollToTop} ref={topRef}>Home</li>
           </Link>
           
           <Link to="/create-post" onClick={scrollToTop}>
-            <li className='cursor-pointer px-3 py-2 rounded-lg bg-blue-600 hover:bg-blue-500 active:scale-95'>New Post</li>
+            <li className='cursor-pointer border  bg-white text-black px-6 py-1 rounded-lg flex '><CiPen style={{fontSize:'20px', marginTop:'4px',marginRight:'6px'}}/> Write</li>
           </Link>
-          <li className='cursor-pointer hover:border border-cyan-600 px-3 py-2 rounded-lg' onClick={handleAuthClick}>
-            {token ? "Profile" : "Sign In"}
+          <li className='rounded-lg flex' onClick={handleAuthClick}>
+            <button className="text-xl flex cursor-pointer">
+            {token ? 
+              <div>
+                {userImage ? (<img src={userImage} alt="" className='rounded-full w-10 h-10 object-cover' />) : <span className='border rounded-full border-transparent bg-gray-900 flex p-2 px-3'><FiUser style={{marginTop:'4px'}}/></span> }
+              </div> : 
+              <span className='flex my-1'><FiUser style={{marginTop:'4px', marginRight:'8px'}}/>Sign In</span> }
+          </button>
           </li>
         </ul>
-        <div className="md:hidden  flex items-center">
-          <button className="text-xl md:hidden" onClick={handleAuthClick}>
-            {token ? "Profile" : "Sign In"}
+        <div className='flex '>
+          <div className='mx-4'>
+          <button className='md:hidden'>
+            <MdOutlineWbSunny style={{marginTop:'10px', fontSize:'20px'}}/>
           </button>
         </div>
+
+        <div className="md:hidden  flex items-center">
+          <button className="text-xl md:hidden flex" onClick={handleAuthClick}>
+            {token ? 
+            <div>
+                {userImage ? (<img src={userImage} alt="" className='rounded-full w-10 h-10 object-cover' />) : <span className='border rounded-full border-transparent bg-gray-900 flex p-2 px-3'><FiUser style={{marginTop:'4px'}}/></span> }
+            </div> : 
+            <span className='flex'><FiUser style={{marginTop:'4px', marginRight:'8px'}}/>Sign In</span> }
+          </button>
+        </div>
+        </div>   
+        
       </div>
     </nav>
   );
