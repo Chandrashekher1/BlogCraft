2a02c03405462b2e7b9fa06282017c1eee860a39
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-18 23:08:50 +0530

    refactor: change design of footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,14 @@ import SocialIcons from './SocialIcon';
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col md:flex-row items-center justify-between px-6 py-6 border-t border-blue-800 bg-black text-white">
+    <footer className="flex flex-col md:flex-row items-center justify-between px-6 py-6 border-t border-gray-800 bg-gray-950 text-white">
       <div className="flex items-center space-x-1 cursor-pointer">
-        <h1 className="font-bold text-2xl">Vibe</h1>
-        <p className="text-blue-500 font-bold text-2xl">Script</p>
+        <p className='bg-gradient-to-r from-white to-gray-700 bg-clip-text text-transparent font-bold text-2xl'>BlogCraft</p>
       </div>
 
       <div className="text-center md:text-left">
-        <p className="font-medium text-lg">
-          Created by Chandrashekher <span className="text-gray-400">@2025</span>
+        <p className="text-gray-400">
+          Created by <span className='font-semibold text-lg'>Chandrashekher</span> <span className="text-gray-400">@2025</span>
         </p>
       </div>
 
