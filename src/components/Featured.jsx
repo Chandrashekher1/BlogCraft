ff7b811dc08ae5b6513309d3f9df8f7a6f267469
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-20 01:49:18 +0530

    style: add duration 300

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -7,7 +7,7 @@ const Featured = () => {
   return (
     <div className='flex flex-col md:flex-row items-center justify-center mx-12 md:mx-40 my-8'>
         <div className='border border-gray-800 bg-gray-900 rounded-md text-center flex flex-col items-center p-4 my-4 md:mx-4 hover:-translate-y-1.5'>
-            <span className='border rounded-full p-2 border-transparent bg-gray-600 hover:bg-gray-500'><IoBookOutline style={{fontSize:'30px'}}/></span>
+            <span className='border rounded-full p-2 border-transparent bg-gray-600 hover:bg-gray-500 hover:duration-300'><IoBookOutline style={{fontSize:'30px'}}/></span>
             <h1 className='font-bold text-xl my-2'>Easy Publishing</h1>
             <p className='text-gray-400'>Create and publish your stories with our intuitive editor and AI assistance</p>
         </div>
