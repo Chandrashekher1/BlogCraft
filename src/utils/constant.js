eb98e52a282ceb3adcf4e50ca79c7b102f4be6b8
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-10-25 11:31:16 +0530

    fix: fix login api issue

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const Login_API = "https://cp-blog.onrender.com//api/login"
+export const Login_API = "https://cp-blog.onrender.com/api/login"
 export const Register_API = "https://cp-blog.onrender.com/api/user"
 export const post_API = "https://cp-blog.onrender.com/api/post"
 export const profile_APi = "https://cp-blog.onrender.com/api/user/me"
