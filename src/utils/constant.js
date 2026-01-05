13e8b4015f72f85fd0cf065c6118bcbc5d39a8d4
Chandrashekher Prasad <146510072+Chandrashekher1@users.noreply.github.com>
2026-01-05 11:49:55 +0530

    Update API base URLs to cp-blog.onrender.com

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,8 @@
-export const Login_API = "https://blogcraft-backend-2uy4.onrender.com/api/login"
-export const Register_API = "https://blogcraft-backend-2uy4.onrender.com/api/user"
-export const post_API = "https://blogcraft-backend-2uy4.onrender.com/api/post"
-export const profile_APi = "https://blogcraft-backend-2uy4.onrender.com/api/user/me"
-export const allPost_API = "https://blogcraft-backend-2uy4.onrender.com/api/post/user"
+export const Login_API = "https://cp-blog.onrender.com/api/login"
+export const Register_API = "https://cp-blog.onrender.com/api/user"
+export const post_API = "https://cp-blog.onrender.com/api/post"
+export const profile_APi = "https://cp-blog.onrender.com/api/user/me"
+export const allPost_API = "https://cp-blog.onrender.com/api/post/user"
 export const sample_image = "https://marketinginsidergroup.com/wp-content/uploads/2023/09/shutterstock_234456937-scaled-1.jpg"
 export const Backgorund_image= "https://res.cloudinary.com/dt9a9xhz1/image/upload/v1750730811/Blog1_sq8rzq.png"
 export const GPT_KEY = import.meta.env.VITE_GPT_API_KEY
